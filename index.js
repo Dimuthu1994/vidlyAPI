aed4217ee76f0efee80364ae52fec7046df42ed7
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-15 09:57:15 +0530

    handling post req

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Thriller" },
+  { id: 3, name: "Romance" },
+  { id: 4, name: "Horror" },
+];
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.post("/api/genres", (req, res) => {
+  const genre = { id: genres.length + 1, name: req.body.name };
+  genres.push(genre);
+  res.send(genre);
 });
 
 const port = process.env.PORT || 3000;
