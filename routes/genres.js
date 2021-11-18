13101896a76ae1db99b28bba5b9a50755a53b64b
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-18 17:44:34 +0530

    restructuring genre

--- routes/genres.js
+++ routes/genres.js
@@ -1,38 +1,26 @@
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const { Genre, validate } = require("../models/customer");
 const express = require("express");
 const router = express.Router();
 //express() not work so we need .Router()
 // when we seperate routes
 
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 10,
-  },
-});
-
-const Genres = mongoose.model("Genre", genreSchema);
-
 router.get("/", async (req, res) => {
-  const genres = await Genres.find().sort("name");
+  const genres = await Genre.find().sort("name");
   res.send(genres);
 });
 
 router.get("/:id", async (req, res) => {
-  const genre = await Genres.findById(req.params.id);
+  const genre = await Genre.findById(req.params.id);
   if (!genre)
     return res.status(404).send("The genre with the given ID is not found");
   res.send(genre);
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genres({ name: req.body.name });
+  let genre = new Genre({ name: req.body.name });
 
   genre = await genre.save();
   res.send(genre);
@@ -41,10 +29,10 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   //validate
   //if invalide return 400 bad req
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genres.findByIdAndUpdate(
+  const genre = await Genre.findByIdAndUpdate(
     req.params.id,
     { name: req.body.name },
     { new: true }
@@ -60,18 +48,11 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   //look up the genres
   //if not exist return 404
-  const genre = await Genres.findByIdAndDelete(req.params.id);
+  const genre = await Genre.findByIdAndDelete(req.params.id);
   if (!genre) return res.status(404).send("The genre with given id not found");
 
   //return the delete genre
   res.send(genre);
 });
-
-function validateGenre(course) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  return schema.validate(course);
-}
 
 module.exports = router;
