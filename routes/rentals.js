5b715c61361c9b9f037f739cc65cb0b2c677fe3f
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-19 20:16:02 +0530

    build the rental api

---
+++ routes/rentals.js
@@ -0,0 +1,52 @@
+const { Rental, validate } = require("../models/rental");
+const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find().sort("-dateOut");
+  res.send(rentals);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send("Invalid customer.");
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid movie.");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock!");
+
+  let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+  // we have 2 seperate operations
+  //it is possible after we save the rentals
+  // something goes wrong may be our server crashes
+  //connection to mongodb drop
+  //perhaps second operation not be complete
+  // thats where we need a transaction
+  // with transaction we can ensure both threse operations
+  // will update the state of data in database or none of them will apply
+  rental = await rental.save();
+  movie.numberInStock--;
+  movie.save();
+
+  res.send(rental);
+});
