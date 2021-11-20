c49c51a6e5f66f51f36f29796fefb46be3fd66cc
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-20 10:57:18 +0530

    registering users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const express = require("express");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
@@ -15,6 +16,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 async function main() {
   try {
