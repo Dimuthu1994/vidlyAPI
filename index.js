f34a6ae7cb5e889f917fd98a6ed7cca21448307f
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-21 07:55:59 +0530

    Authenticating users

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
@@ -17,6 +18,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 async function main() {
   try {
