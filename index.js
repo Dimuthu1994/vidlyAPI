08d9fa1c90e825ebd2b8f15467b98c3a5eed8054
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-21 19:58:31 +0530

    extracting routes

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 require("express-async-errors");
 const winston = require("winston");
-const error = require("./middleware/error");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const express = require("express");
-const helmet = require("helmet");
 const mongoose = require("mongoose");
 const app = express();
+require("./startup/routes")(app);
 const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 
-winston.handleException(
-  new winston.transports.File({ filename: "uncaughtException.log" })
+winston.handleExceptions(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
 );
 process.on("unhandledRejection", (ex) => {
   throw ex;
@@ -28,20 +21,6 @@ if (!config.get("jwtPrivateKey")) {
   console.log("FATAL ERROR:jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-const p = Promise.reject(new Error("Something failed miserably"));
-p.then(() => console.log("Done"));
-
-app.use(helmet());
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error); //not calling passing ref to function
 
 async function main() {
   try {
---
+++ startup/routes.js
@@ -0,0 +1,22 @@
+const error = require("../middleware/error");
+const genres = require("../routes/genres");
+const customers = require("../routes/customers");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const helmet = require("helmet");
+const express = require("express");
+
+module.exports = function (app) {
+  app.use(helmet());
+  app.use(express.json());
+  app.use("/api/genres", genres);
+  app.use("/api/customers", customers);
+  app.use("/api/movies", movies);
+  app.use("/api/rentals", rentals);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+
+  app.use(error); //not calling passing ref to function
+};
