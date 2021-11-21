f1620ceea17655965bb8db5b6c435a394662bdb0
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-21 20:13:59 +0530

    extracting login logic

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-require("express-async-errors");
-const winston = require("winston");
 const express = require("express");
-
 const app = express();
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
 
 if (!config.get("jwtPrivateKey")) {
   console.log("FATAL ERROR:jwtPrivateKey is not defined.");
