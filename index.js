d39520c755f8ce307cd576501369c339a1a64cf4
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-21 19:22:44 +0530

    winton handle exption to handle uncaughtExceptions

--- index.js
+++ index.js
@@ -15,16 +15,11 @@ const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 
-process.on("uncaughtException", (ex) => {
-  console.log("WE GOT AN UNCAUGHT EXCEPTION");
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
+winston.handleException(
+  new winston.transports.File({ filename: "uncaughtException.log" })
+);
 process.on("unhandledRejection", (ex) => {
-  console.log("WE GOT AN UNHANDLED REJECTION");
-  winston.error(ex.message, ex);
-  process.exit(1);
+  throw ex;
 });
 
 winston.add(winston.transports.File, { filename: "logfile.log" });
