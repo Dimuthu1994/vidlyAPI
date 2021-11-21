0426cacd9aedd943154e4d0a265fcd938357fc61
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-21 20:34:29 +0530

    exteacting validation logic

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const winston = require("winston");
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
-const Joi = require("@hapi/joi");
-Joi.objectId = require("joi-objectid")(Joi);
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
